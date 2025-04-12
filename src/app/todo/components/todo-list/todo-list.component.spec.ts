7cd3be890ce427b6b033278fe5e2b9ea4f04d04f
Ghazanfar <ghazanfar.ali.0347@gmail.com>
2025-04-12 04:05:17 +0200

    (Test): Test cases included

--- src/app/todo/components/todo-list/todo-list.component.spec.ts
+++ src/app/todo/components/todo-list/todo-list.component.spec.ts
@@ -1,22 +1,62 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TodoListComponent } from './todo-list.component';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { FormsModule } from '@angular/forms';
+import { TodoState } from '../../store/todo.reducer';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular Material modules used in the component
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 
 describe('TodoListComponent', () => {
   let component: TodoListComponent;
   let fixture: ComponentFixture<TodoListComponent>;
+  let store: MockStore;
+
+  const initialState: { todos: TodoState } = {
+    todos: {
+      todos: [],
+      loading: false,
+      error: null,
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TodoListComponent],
+      imports: [
+        FormsModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        CdkAccordionModule,
+      ],
+      providers: [provideMockStore({ initialState })],
     }).compileComponents();
 
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(TodoListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add a list and reset newListTitle', () => {
+    const dispatchSpy = spyOn(store, 'dispatch');
+    component.newListTitle = 'Test List';
+    component.addList();
+    expect(dispatchSpy).toHaveBeenCalled();
+    expect(component.newListTitle).toBe('');
   });
 });
--- src/app/todo/services/todo.service.spec.ts
+++ src/app/todo/services/todo.service.spec.ts
@@ -1,16 +1,47 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TodoService } from './todo.service';
+import { Task, Todo } from '../models/todo.model';
 
 describe('TodoService', () => {
   let service: TodoService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [TodoService],
+    });
+
     service = TestBed.inject(TodoService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should fetch to-do lists', () => {
+    const mockLists: Todo[] = [{ id: '1', title: 'Test List', tasks: [] }];
+
+    service.getTodos().subscribe((lists) => {
+      expect(lists).toEqual(mockLists);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/lists');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockLists);
+  });
+
+  it('should add a new to-do list', () => {
+    const title = 'New List';
+
+    service.addTodo(title).subscribe((response) => {
+      expect(response).toEqual({ id: '123', title });
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/lists');
+    expect(req.request.method).toBe('POST');
+    req.flush({ id: '123', title });
   });
 });
